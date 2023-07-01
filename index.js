60afc2ffcf5ef9ff32b57be4a760f869c383c221
awmorrell <amorrell424@gmail.com>
2023-07-01 12:24:02 -0700

    shifting background gradient, related news, and fetch abort features

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('.container');
+const containerTwo = document.querySelector('.container-two');
 const searchButton = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const timeDetails = document.querySelector('.time-details');
+const localNews = document.querySelector('.local-news');
+const articleOne = document.querySelector('.article-one');
+const articleTwo = document.querySelector('.article-two');
+const articleThree = document.querySelector('.article-three');
+const newsList = document.querySelector('.news-list');
+const pElement = document.createElement('p');
+	pElement.textContent = "Sorry, couldn't find related news."
 const error = document.querySelector('.not-found');
-const body = document.body;
+const body = document.querySelector('.body');
 
 function handleKeyPress(event) {
   if (event.key === 'Enter') {
@@ -17,7 +25,7 @@ function handleKeyPress(event) {
 document.addEventListener('keypress', handleKeyPress);
 
 searchButton.addEventListener('click', () => {
-	const city = document.querySelector('.search-box input').value;
+	const city = document.querySelector('.search-box input').value.toLowerCase();
 
 	if (city === '')
 		return;
@@ -25,6 +33,8 @@ searchButton.addEventListener('click', () => {
 	weatherBox.classList.remove('fade-in');
 	weatherDetails.classList.remove('fade-in');
 	timeDetails.classList.remove('fade-in');
+	localNews.classList.remove('fade-in');
+	containerTwo.classList.remove('fade-in');
 
   const requestOptions = {
     method: 'GET'
@@ -34,11 +44,20 @@ searchButton.addEventListener('click', () => {
 		.then(response => response.json())
 		.then(json => {
 
+			articleOne.innerHTML = '';
+			articleTwo.innerHTML = '';
+			articleThree.innerHTML = '';
+
+			if(pElement.parentElement === newsList) {
+					newsList.removeChild(pElement)
+			};
+
 			if (json.cod === '404') {
 				container.classList.add('enlarge');
 				weatherBox.style.display = 'none';
 				weatherDetails.style.display = 'none';
 				timeDetails.style.display = 'none';
+				localNews.style.display = 'none';
 				error.style.display = 'block';
 				error.classList.add('fade-in');
 				return;
@@ -80,19 +99,19 @@ searchButton.addEventListener('click', () => {
 						localtime = `${localHours}:${localMinutes}` + ' PM';
 					} else if (Number(localHours) > 12) {
 						localtime = `${localHours - 12}:${localMinutes}` + ' PM';
-					} else if(Number(localHours) < 12) {
+					} else if (Number(localHours) < 12 && Number(localHours) > 0) {
 						localtime = `${localHours}:${localMinutes}` + ' AM';
-					}		
+					} else if (Number(localHours) === 0) {
+						localtime = `12:${localMinutes}` + ' AM';
+					}
 
 			//Day or Night
 			if (currentTime > sunsetDate || currentTime < sunriseDate) {
-				body.classList.add('day-to-night');
-				body.classList.remove('night-to-day');
+				body.classList.add('faade-in');
 			}
 
-			if ((currentTime < sunsetDate && currentTime > sunriseDate) && body.classList.contains('day-to-night')) {
-				body.classList.remove('day-to-night');
-				body.classList.add('night-to-day');
+			if ((currentTime < sunsetDate && currentTime > sunriseDate) && body.classList.contains('faade-in')) {
+				body.classList.remove('faade-in');
 			}
 
 
@@ -160,12 +179,104 @@ searchButton.addEventListener('click', () => {
 			weatherBox.style.display = '';
 			weatherDetails.style.display = '';
 			timeDetails.style.display = '';
+			localNews.style.display = '';
+			containerTwo.style.display = '';
 			weatherBox.classList.add('fade-in');
 			weatherDetails.classList.add('fade-in');
 			timeDetails.classList.add('fade-in');
+			localNews.classList.add('fade-in');
+			containerTwo.classList.add('fade-in');
 			container.classList.add('enlarge');
 
 			console.log(json);
 
-		});
+	function getCurrentDate() {
+  	const now = new Date();
+  	now.setDate(now.getDate() - 3); // Subtract one day
+
+  	let year = now.getFullYear();
+  	let month = String(now.getMonth() + 1).padStart(2, '0');
+  	let day = String(now.getDate()).padStart(2, '0');
+
+  	if (day <= '00') {
+    // If day becomes zero, set it to 28
+   		day = '28';
+  	}
+
+  	return `${year}-${month}-${day}`;
+	}
+
+	const currentDate = getCurrentDate();
+	console.log(currentDate);
+
+	const country = json.sys.country.toLowerCase();
+	
+	articleOne.innerHTML = "Searching...";
+
+	let controller;
+	let signal;
+
+	function makeFetchRequest() {
+  	if (controller) {
+    // Abort the previous request
+    controller.abort();
+  	}
+
+ 			controller = new AbortController();
+  		signal = controller.signal;
+
+			fetch(`https://api.worldnewsapi.com/search-news?api-key=2eab0e76597441efbb5e1da1361a30aa&text=${encodeURIComponent(city)}&source-countries=${encodeURIComponent(country)}&earliest-publish-date=${encodeURIComponent(currentDate)}`, { signal })
+				.then(response => response.json())
+				.then(json => {
+					console.log(json);
+					if(json.news.length >= 3) {
+				    const articles = json.news.slice(0, 3);
+				    	if (articles[0].title.length === 0) {
+				    		articleOne.innerHTML = '';
+				    	} else {
+				    	articleOne.innerHTML = "- " + articles[0].title;
+				    		articleOne.setAttribute('href', articles[0].url);				    		
+				    	}
+				    	if (articles[1].title.length === 0) {
+				    		articleTwo.innerHTML = '';
+				    	} else {
+				    	articleTwo.innerHTML = "- " + articles[1].title;
+				    		articleTwo.setAttribute('href', articles[1].url);				    		
+				    	}
+				    	if (articles[2].title.length === 0) {
+				    		articleThree.innerHTML = '';
+				    	} else {
+				    	articleThree.innerHTML = "- " + articles[2].title;
+				    		articleThree.setAttribute('href', articles[2].url);				    		
+				    	}			
+					} else if(json.news.length === 2) {
+							const articles = json.news.slice(0, 2);
+				    	if (articles[0].title.length === 0) {
+				    		articleOne.innerHTML = '';
+				    	} else {
+				    	articleOne.innerHTML = "- " + articles[0].title;
+				    		articleOne.setAttribute('href', articles[0].url);				    		
+				    	}
+				    	if (articles[1].title.length === 0) {
+				    		articleTwo.innerHTML = '';
+				    	} else {
+				    	articleTwo.innerHTML = "- " + articles[1].title;
+				    		articleTwo.setAttribute('href', articles[1].url);				    		
+				    	}
+					} else if (json.news.length === 1) {
+							const articles = json.news[0];
+				    	if (articles[0].title.length === 0) {
+				    		articleOne.innerHTML = '';
+				    	} else {
+				    	articleOne.innerHTML = "- " + articles[0].title;
+				    		articleOne.setAttribute('href', articles[0].url);				    		
+				    	}
+					}	else if (json.news.length === 0) {
+							articleOne.innerHTML = "";
+							newsList.appendChild(pElement);
+					}	
+				})
+	}
+	makeFetchRequest();
+	})
 })
